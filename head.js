214df2415c861a8cb2805fa7928b2c415ba49162
Jomel Edodollon <jomeledodollon@yahoo.ca>
2022-01-11 17:24:07 -0800

    Commented out test cases

--- head.js
+++ head.js
@@ -10,7 +10,8 @@ const head = (array) => {
   return array[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5]), 5);
-assertEqual(head([]), undefined);
+// TEST CODE
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([5]), 5);
+// assertEqual(head([]), undefined);
