7b26daf88808ff89fb85acc4f02996d904ca05aa
Jomel Edodollon <jomeledodollon@yahoo.ca>
2022-01-11 19:46:16 -0800

    Commented out test cases

--- eqArrays.js
+++ eqArrays.js
@@ -15,10 +15,10 @@ const eqArrays = function(firstArray, secondArray) {
 };
 
 // TEST CASES
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => falsconsole.loge
+// console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+// console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => falsconsole.loge
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => falseconsole.log
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => falseconsole.log
