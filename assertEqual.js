cc37de866159eb79da6aea5d4af5bf311b849907
Jomel Edodollon <jomeledodollon@yahoo.ca>
2022-01-17 15:40:47 -0800

    Refactored assertEqual and head by separating tests and fucntions into separate modules. Also refactored assertEqual if..statements into a ternary operator

--- assertEqual.js
+++ assertEqual.js
@@ -1,13 +1,6 @@
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
+  actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
 
-// TEST CODE
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-// assertEqual("Jomel", "Jomel");
-// assertEqual(32, -32);
+module.exports = assertEqual;
