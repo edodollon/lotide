1be72106283a1bb1b6b38e62a1a618652228ad7a
Jomel Edodollon <jomeledodollon@yahoo.ca>
2022-01-17 17:32:15 -0800

    Refactored middleTest and tailTest to use mocha and chai for assertion tests

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,12 +1,45 @@
-const eqArrays = require('../eqArrays');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
+describe('#middle', () => {
+  it('Return empty array if array length passed in is < 2', () => {
+    const actual = middle([1]);
+    const expected = [];
+    assert.deepEqual(actual, expected);
+  });
+  it('Return empty array if array length passed in is < 2', () => {
+    const actual = middle([1, 2]);
+    const expected = [];
+    assert.deepEqual(actual, expected);
+  });
+  it('Return middle number if array is odd', () => {
+    const actual = middle([1, 2, 3]);
+    const expected = [2];
+    assert.deepEqual(actual, expected);
+  });
+  it('Return middle number if array is odd', () => {
+    const actual = middle([1, 2, 3, 4, 5]);
+    const expected = [3];
+    assert.deepEqual(actual, expected);
+  });
+  it('Return 2 middle numbers if array is even', () => {
+    const actual = middle([1, 2, 3, 4]);
+    const expected = [2, 3];
+    assert.deepEqual(actual, expected);
+  });
+  it('Return 2 middle numbers if array is even', () => {
+    const actual = middle([1, 2, 3, 4, 5, 6]);
+    const expected = [3, 4];
+    assert.deepEqual(actual, expected);
+  });
+});
+
 // TEST CASES
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
+// console.log(middle([1])); // => []
+// console.log(middle([1, 2])); // => []
 
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
+// console.log(middle([1, 2, 3])); // => [2]
+// console.log(middle([1, 2, 3, 4, 5])); // => [3]
 
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(eqArrays(middle([1, 2, 3, 4, 5, 6]), [3, 4])); // => [3, 4]
+// console.log(middle([1, 2, 3, 4])); // => [2, 3]
+// console.log(eqArrays(middle([1, 2, 3, 4, 5, 6]), [3, 4])); // => [3, 4]
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,14 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-console.log(words);
+describe('#tail', () => {
+  it ('Returns tail', () => {
+    const actual = tail(["Yo Yo", "Lighthouse", "Labs"]);
+    const expected = [ 'Lighthouse', 'Labs' ];
+    assert.deepEqual(actual, expected);    
+  })
+  it ('Original array is not changed', () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    assert.strictEqual(words.length, 3);    
+  })
+});
